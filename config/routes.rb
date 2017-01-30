db7a3307944754dfb4e9b82029ba9f37186b37cb
Graham Moss <graham.moss@hotmail.com>
2017-01-30 15:31:56 -0500

    Adding feature to delete pictures

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
   patch 'pictures/:id' => "pictures#update"
 
+  delete 'picture/:id' => 'pictures#destroy', as: "delete_picture"
+
+  root 'pictures#index'
+
 
   get 'pictures/:id' => 'pictures#show', as: 'picture'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
